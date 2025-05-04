5be35c784634bd288a94a8649f409fcda5033746
OliverDevsCode <186149954+OliverDevsCode@users.noreply.github.com>
2025-05-04 11:44:53 +0100

    background image

---
+++ README.md
@@ -0,0 +1,8 @@
+# SQL Mini Full Stack App
+
+- Installation
+
+cd whole folder npm install
+cd client npm install
+
+cd whole folder npm start
--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import LoginForm from './components/LoginForm/LoginForm';
 
 function App() {
 
+  //testing that server works
   fetch('/students/selectAll')
     .then(res => res.json())
     .then(data => {
         console.log('Results:', data);  // Log the data to the console
     })
     .catch(err => console.log('Error:', err));  // Log any errors if the fetch fails
-
+  //test ends   
 
   return (
     <div className="App">
