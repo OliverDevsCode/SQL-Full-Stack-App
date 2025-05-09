1b126c74260c5a682f35364cd23d2f65f72f8d7a
Your Name <186149954+OliverDevsCode@users.noreply.github.com>
2025-05-09 18:46:48 +0100

    removing env template

--- server/database.js
+++ server/database.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql');
 
 var db = mysql.createConnection({
-  host: process.env.HOST,
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect(function(err) {
