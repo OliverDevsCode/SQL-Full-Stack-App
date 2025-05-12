e31abf5d6293b2f19341a0ae388817f866b66389
OliverDevsCode <186149954+OliverDevsCode@users.noreply.github.com>
2025-05-12 21:16:35 +0100

    easier free period identifaction

--- client/src/components/LessonCard/LessonCard.jsx
+++ client/src/components/LessonCard/LessonCard.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import './LessonCard.css';
 
 const LessonCard = ({ period, subject, teacher, location }) => {
+
+  if(subject ==='Free Period'){
+    return (
+    <div className="lesson-card-free-card">
+      <h4>Period {period}</h4>
+      <h3>{subject}</h3>
+      <p>{teacher}</p>
+      <small>Room {location}</small>
+    </div>
+  );
+  }
+
+  
   return (
     <div className="lesson-card">
       <h4>Period {period}</h4>
